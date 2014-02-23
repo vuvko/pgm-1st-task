1f5e894d404f12465f9dbe44bf475e5b04de5165
Andrey Shadrikov <notvuvko@gmail.com>
2014-02-23 11:09:01 +0400

    First commit.

---
+++ init3.m
@@ -0,0 +1,10 @@
+% Инициализация параметров для варианта 3
+
+params.a_min = 15;
+params.a_max = 30;
+params.b_min = 250;
+params.b_max = 350;
+params.p1 = 0.5;
+params.p2 = 0.05;
+params.p3 = 0.5;
+params.N = 50;
---
+++ p4_c.m
@@ -0,0 +1,31 @@
+% Функция 
+% 
+% Входящие параметры:
+%   params - структура с полями a_min, a_max, b_min, b_max, p1, p2
+% Выходящие значения:
+%   p - распределение случайной величины (вектор)
+%   m - математическое ожидание
+%   d - дисперсия
+
+function [c, p, m, d] = p4_c(params)
+  a_size = params.a_max - params.a_min + 1;
+  b_size = params.b_max - params.b_min + 1;
+  c_min = 0;
+  c_max = params.a_max + params.b_max;
+  c_size = c_max - c_min + 1;
+  c = [c_min:c_max]';
+  p = zeros(c_size, 1);
+  lambda = repmat([params.a_min:params.a_max]', 1, b_size) * params.p1 + ...
+      repmat([params.b_min:params.b_max], a_size, 1) * params.p2;
+  pa = 1/a_size;
+  pb = 1/b_size;
+  fact = 1;
+  t = exp(-lambda);
+  for i = 1:3*max(max(lambda))+1
+    p(i) = pa * pb * sum(sum(t)) / fact;
+    fact = fact * i;
+    t = t .* lambda;
+  end
+  m = 0;
+  d = 0;
+end
